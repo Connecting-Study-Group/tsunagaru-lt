6c991089c9e3b3f09c47b3484fc3c81650f6644c
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-18 10:10:32 +0900

    feat: 共通部分の実装

---
+++ src/components/system/Spacer.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import { Box } from "@mantine/core";
+
+export const Spacer = memo(() => {
+  return (
+    <Box
+      role="presentation"
+      sx={{
+        flex: "1 1 0%",
+        placeSelf: "stretch",
+      }}
+    ></Box>
+  );
+});
