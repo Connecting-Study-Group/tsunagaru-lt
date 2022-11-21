6bbffea8cbbd9bbc4d9dcfc6d5d6d26b4de0fa22
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-21 15:09:58 +0900

    fix: topページの実装修正

---
+++ src/components/pages/IndexPage.tsx
@@ -0,0 +1,56 @@
+import React, { memo } from "react";
+import Image from "next/image";
+import { Text, Paper, Box, Button } from "@mantine/core";
+import { Spacer } from "@/components/system/Spacer";
+import { useAuth } from "@/hooks/useAuth";
+import Link from "next/link";
+
+export const IndexPage = memo(() => {
+  const { user } = useAuth();
+  return (
+    <>
+      <Text component="h1" sx={{ visibility: "hidden" }}>
+        つながるLT
+      </Text>
+      <Box sx={{ display: "flex" }}>
+        <Spacer />
+        <Paper shadow="md" p="md" radius={16} sx={{ display: "inline-flex" }}>
+          <Image
+            src="/images/logo.svg"
+            width={120}
+            height={120}
+            alt="つながるLT"
+          />
+        </Paper>
+        <Spacer />
+      </Box>
+      <Text sx={{ marginTop: 32 }}>
+        「つながるLT」はつながる勉強会のコミュニティ内で使用できる資料共有サービスです。
+      </Text>
+      {/* 認証後ボタンを表示 */}
+      {user ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "32px",
+          }}
+        >
+          <Link href="/dashboard" passHref>
+            <Button
+              component="a"
+              variant="gradient"
+              gradient={{ from: "teal", to: "lime", deg: 105 }}
+            >
+              支払いページを見る
+            </Button>
+          </Link>
+        </Box>
+      ) : (
+        <Link href="/login" passHref>
+          <Text>ログイン画面へ</Text>
+        </Link>
+      )}
+    </>
+  );
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,13 @@
-import { Layout } from "@/components/system/Layout";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { Text, Paper, Box, Button } from "@mantine/core";
-import { Spacer } from "@/components/system/Spacer";
-import { useAuth } from "@/hooks/useAuth";
-import Link from "next/link";
+import { Layout } from "@/components/template/Layout";
+import { ReactElement } from "react";
+import { IndexPage } from "@/components/pages/IndexPage";
 
-const Top: NextPage = () => {
-  const { user } = useAuth();
-  return (
-    <Layout>
-      <Head>
-        <title>つながるLT</title>
-      </Head>
-
-      <main>
-        <Text component="h1" sx={{ visibility: "hidden" }}>
-          つながるLT
-        </Text>
-        <Box sx={{ display: "flex" }}>
-          <Spacer />
-          <Paper shadow="md" p="md" radius={16} sx={{ display: "inline-flex" }}>
-            <Image
-              src="/images/logo.svg"
-              width={120}
-              height={120}
-              alt="つながるLT"
-            />
-          </Paper>
-          <Spacer />
-        </Box>
-        <Text sx={{ marginTop: 32 }}>
-          「つながるLT」はつながる勉強会のコミュニティ内で使用できる資料共有サービスです。
-        </Text>
-        {/* 認証後ボタンを表示 */}
-        {user ? (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "32px",
-            }}
-          >
-            <Link href="/dashboard" passHref>
-              <Button
-                component="a"
-                variant="gradient"
-                gradient={{ from: "teal", to: "lime", deg: 105 }}
-              >
-                支払いページを見る
-              </Button>
-            </Link>
-          </Box>
-        ) : (
-          <Link href="/login" passHref>
-            <Text>ログイン画面へ</Text>
-          </Link>
-        )}
-      </main>
-    </Layout>
-  );
+const Index = (): JSX.Element => {
+  return <IndexPage />;
 };
 
-export default Top;
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Index;
