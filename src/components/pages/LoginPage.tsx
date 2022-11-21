45057b69ddea85adae3253a06698df29a48d47d3
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-21 15:09:43 +0900

    fix: loginページの実装修正

---
+++ src/components/pages/LoginPage.tsx
@@ -0,0 +1,37 @@
+import React, { memo, useEffect, useState } from "react";
+import { Text, Paper, Box, Button } from "@mantine/core";
+import { useAuth } from "@/hooks/useAuth";
+import { LogoSlack } from "react-ionicons";
+import SystemHelper from "@/helpers/system";
+
+const BASE_URL = `https://slack.com/oauth/authorize?scope=team:read,users:read&client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}&state=`;
+
+export const LoginPage = memo(() => {
+  const { isLoading } = useAuth();
+  const [slackAuthUrl, setSlackAuthUrl] = useState(BASE_URL);
+  // slack認証用のURL
+  useEffect(() => {
+    if (SystemHelper.isBrowser)
+      setSlackAuthUrl(`${BASE_URL}${window.location.href}`);
+  }, []);
+  return (
+    <>
+      <Text component="h1">ログイン</Text>
+      <Text>
+        slackのコミュニティに所属しているメンバーであればログイン可能です
+      </Text>
+      <Button
+        component="a"
+        href={slackAuthUrl}
+        variant="outline"
+        leftIcon={<LogoSlack />}
+        loading={isLoading}
+        sx={{ backgroundColor: "white", border: "solid 1px black" }}
+        size="md"
+        color="dark"
+      >
+        Slackでログイン
+      </Button>
+    </>
+  );
+});
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,46 +1,13 @@
-import { Layout } from "@/components/system/Layout";
-import type { NextPage } from "next";
-import React, { useEffect, useState } from "react";
-import { Text, Paper, Box, Button } from "@mantine/core";
-import Head from "next/head";
-import { useAuth } from "@/hooks/useAuth";
-import { LogoSlack } from "react-ionicons";
+import { Layout } from "@/components/template/Layout";
+import React, { ReactElement } from "react";
+import { LoginPage } from "@/components/pages/LoginPage";
 
-const BASE_URL = `https://slack.com/oauth/authorize?scope=team:read,users:read&client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}&state=`;
+const Login = (): JSX.Element => {
+  return <LoginPage />;
+};
 
-const Login: NextPage = () => {
-  const { isLoading } = useAuth();
-  const [slackAuthUrl, setSlackAuthUrl] = useState(BASE_URL);
-  // slack認証用のURL
-  useEffect(() => {
-    if (typeof window !== "undefined")
-      setSlackAuthUrl(`${BASE_URL}${window.location.href}`);
-  }, []);
-  return (
-    <Layout>
-      <Head>
-        <title>ログイン | つながるLT</title>
-      </Head>
-      <main>
-        <Text component="h1">ログイン</Text>
-        <Text>
-          slackのコミュニティに所属しているメンバーであればログイン可能です
-        </Text>
-        <Button
-          component="a"
-          href={slackAuthUrl}
-          variant="outline"
-          leftIcon={<LogoSlack />}
-          loading={isLoading}
-          sx={{ backgroundColor: "white", border: "solid 1px black" }}
-          size="md"
-          color="dark"
-        >
-          Slackでログイン
-        </Button>
-      </main>
-    </Layout>
-  );
+Login.getLayout = function getLayout(page: ReactElement) {
+  return <Layout title="ログイン">{page}</Layout>;
 };
 
 export default Login;
