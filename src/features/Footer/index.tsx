0fffbf489b5265e95ca1e0e42527bd58c1ceec17
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-11-30 23:28:09 +0900

    feat: Create Default Layout

---
+++ src/features/Footer/index.tsx
@@ -0,0 +1,8 @@
+import { Footer } from '@mantine/core'
+import React, { memo } from 'react'
+
+export const AppFooter = memo(() => {
+    return (
+      <Footer height={60} p="xs">AppFooter</Footer>
+    )
+  })
