086756abdac3bf09cce81bdcd05b86f17fdbe12c
sunadoi <y.sunadoi@gmail.com>
2022-12-11 17:12:06 +0900

    chore: api key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,8 @@ import type { FirebaseOptions } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // envファイルを読み込まないCI上で空文字やundefinedだとbuildに失敗するため、適当な文字列を入れておく
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "sample",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
